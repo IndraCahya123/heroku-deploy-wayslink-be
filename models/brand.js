9cb0bab6736627eb24c4a2dd6377145ca3e998bc
IndraCahya123 <indracahya51@gmail.com>
2021-05-03 20:16:33 +0700

    fixing brands table migration

--- models/brand.js
+++ models/brand.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       Brand.belongsTo(models.User, {
         foreignKey: "userId",
-        as: "user",
+        as: "userId",
         onUpdate: "CASCADE",
         onDelete: "CASCADE"
       });
