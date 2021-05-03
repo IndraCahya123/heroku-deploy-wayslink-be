a3e1f26baac5329fda9d63c04c4c1781de411be7
IndraCahya123 <indracahya51@gmail.com>
2021-05-03 20:39:24 +0700

    fixing brands post request

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const router = require("./src/router/main-root");
 const cors = require("cors");
+const router = require("./src/router/main-root");
 
 require("dotenv").config();
 
